b8adf65dc203775caea20bbb21cafb3fa08bd8d4
Iuliia_Mykhailenko <Iuliia.Mykhailenko@iii.com>
2018-03-29 17:52:15 +0300

    Initial commit

---
+++ src/main/java/Robert.java
@@ -0,0 +1,2 @@
+public class Robert extends Person {
+}
---
+++ src/test/java/Runner.java
@@ -0,0 +1,7 @@
+public class Runner {
+    public static void main(String[] args) {
+        Robert r = new Robert();
+        Person per = new Robert();
+        per = r;
+    }
+}
